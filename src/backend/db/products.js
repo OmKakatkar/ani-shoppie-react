4461891d4656dd1774abfc991490c70769c616f4
Om Kakatkar <omkakatkar@gmail.com>
2022-03-22 22:08:13 +0530

    FEAT: Add Wishlist page UI

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -1,30 +1,35 @@
-import { v4 as uuid } from "uuid";
+import { v4 as uuid } from 'uuid';
 
 /**
- * Product Database can be added here.
- * You can add products of your wish with different attributes
+ * TODO: Add more products
  * */
 
 export const products = [
-  {
-    _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
-  },
-  {
-    _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
-  },
-  {
-    _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
-  },
+	{
+		_id: uuid(),
+		title: 'Caffè latte',
+		price: '219',
+		discount: '10',
+		categoryName: 'coffee',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, incidunt?'
+	},
+	{
+		_id: uuid(),
+		title: 'Caffè latte',
+		price: '219',
+		discount: '10',
+		categoryName: 'coffee',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, incidunt?'
+	},
+	{
+		_id: uuid(),
+		title: 'Caffè latte',
+		price: '219',
+		discount: '10',
+		categoryName: 'coffee',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, incidunt?'
+	}
 ];
---
+++ src/pages/Wishlist.js
@@ -0,0 +1,26 @@
+import { products } from '../backend/db/products';
+import EcommerceCard from '../components/EcommerceCard';
+
+function Wishlist() {
+	return (
+		<main className="ecom-main">
+			<h1 className="text-xhuge text-center">Wishlist</h1>
+			<div className="container flex-container">
+				{/* FIXME: Remove product array and fetch wishlist from backend */}
+				{products.map(product => (
+					<EcommerceCard
+						key={product._id}
+						title={product.title}
+						description={product.description}
+						price={product.price}
+						discount={product.discount}
+					>
+						<button className="btn bg-blue rounded">Move to Cart</button>
+					</EcommerceCard>
+				))}
+			</div>
+		</main>
+	);
+}
+
+export default Wishlist;
