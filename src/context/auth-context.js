dc9ad0dcd2b42c6437e50b352ea0d0eec1a0de2d
Om Kakatkar <omkakatkar@gmail.com>
2022-03-24 13:07:13 +0530

    [FEAT] Add AuthProvider with Login and Logout

---
+++ src/context/auth-context.js
@@ -0,0 +1,56 @@
+import { createContext, useContext, useState } from 'react';
+import { login, signup } from '../util/auth-request';
+
+const AuthContext = createContext();
+
+const currentUser = 'ANI_SHOPPIE_USER';
+
+const AuthProvider = ({ children }) => {
+	const initialUser = JSON.parse(localStorage.getItem(currentUser)) || {};
+	const [user, setUser] = useState(initialUser);
+
+	const handleLogin = async ({ email, password }) => {
+		try {
+			const {
+				data: { foundUser: user, encodedToken: token }
+			} = await login({ email, password });
+			if (token) {
+				localStorage.setItem(currentUser, JSON.stringify({ user, token }));
+				setUser(JSON.parse(localStorage.getItem(currentUser)));
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	const handleSignUp = async ({ name, email, password }) => {
+		try {
+			const {
+				data: { user, encodedToken }
+			} = await signup({ name, email, password });
+			if (encodedToken) {
+				localStorage.setItem(currentUser, JSON.stringify({ user }));
+				setUser(JSON.parse(localStorage.getItem(currentUser)));
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	const handleLogout = () => {
+		localStorage.removeItem(currentUser);
+		setUser({});
+	};
+
+	return (
+		<AuthContext.Provider
+			value={{ user, handleLogin, handleLogout, handleSignUp }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { useAuth, AuthProvider };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import { useAuth } from '../context/auth-context';
 import './Auth.css';
 
 function Login() {
+	const { handleLogin } = useAuth();
+	const initialLoginData = {
+		email: '',
+		password: ''
+	};
+	const [loginData, setLoginData] = useState(initialLoginData);
+
+	const handleChange = e => {
+		setLoginData({ ...loginData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		await handleLogin(loginData);
+	};
+
 	return (
 		<main className="flex-container ecom-main">
-			<div className="form-container flex-container flex-column">
+			<form
+				className="form-container flex-container flex-column"
+				onSubmit={handleSubmit}
+			>
 				<h1 className="text-xhuge form-heading">Login</h1>
 				<div className="input-container">
 					<label htmlFor="email"> Email </label>
@@ -13,6 +34,8 @@ function Login() {
 						id="email"
 						placeholder="email@xyz.com"
 						className="input text-md"
+						value={loginData.email}
+						onChange={handleChange}
 					/>
 				</div>
 
@@ -23,6 +46,8 @@ function Login() {
 						name="password"
 						id="password"
 						className="input text-md"
+						value={loginData.password}
+						onChange={handleChange}
 					/>
 				</div>
 				<div className="input-container input-wrap">
@@ -36,7 +61,7 @@ function Login() {
 						<div className="checkbox-icon"></div>
 						Remember me
 					</label>
-					<a href="#" className="form-link">
+					<a href="./" className="form-link">
 						Forgot Password?
 					</a>
 				</div>
@@ -46,7 +71,7 @@ function Login() {
 				<a href="./signup.html" className="form-link">
 					Create an account
 				</a>
-			</div>
+			</form>
 		</main>
 	);
 }
