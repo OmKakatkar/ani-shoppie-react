df74f9dca29124c6ae80392594a4cc07da76c205
Om Kakatkar <omkakatkar@gmail.com>
2022-03-24 20:28:11 +0530

    [WIP] Add all routes

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import { makeServer } from './server';
 import { AuthProvider } from './context/auth-context';
+import { BrowserRouter } from 'react-router-dom';
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
 	<React.StrictMode>
-		<AuthProvider>
-			<App />
-		</AuthProvider>
+		<BrowserRouter>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
