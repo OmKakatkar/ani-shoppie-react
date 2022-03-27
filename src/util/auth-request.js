e3993bf479731893db671c8104dde764aacf1e59
Om Kakatkar <omkakatkar@gmail.com>
2022-03-27 23:48:38 +0530

    [WIP] Add api requests with states

--- src/util/auth-request.js
+++ src/util/auth-request.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
+import { API_LOGIN, API_SIGNUP } from '../constants/api-constants';
 
 /*
  *  Login and SignUp functions to communicate with mock backend
  */
 
 const login = async ({ email, password }) =>
-	await axios.post('/api/auth/login', {
+	await axios.post(API_LOGIN, {
 		email,
 		password
 	});
 
 const signup = async ({ name, email, password }) =>
-	await axios.post('/api/auth/signup', {
+	await axios.post(API_SIGNUP, {
 		name,
 		email,
 		password
